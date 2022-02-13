3eb35592fb82d9d8923056f08b2378b0b20b9e11
idk <hankhill19580@gmail.com>
2022-02-13 01:33:41 -0500

    Better way of doing the bindings

--- goi2pd/goapi.go
+++ goi2pd/goapi.go
@@ -8,7 +8,7 @@ import (
 
 // #cgo CFLAGS: -I${SRCDIR}/../i2pd/libi2pd_wrapper
 // #cgo CXXFLAGS: -I${SRCDIR}/../i18n -I${SRCDIR}/../libi2pd_client -I${SRCDIR}/../libi2pd -g -Wall -Wextra -Wno-unused-parameter -pedantic -Wno-psabi -fPIC -D__AES__ -maes
-// #cgo LDFLAGS: -L${SRCDIR}/../ -l:../i2pd/libi2pdclient.a -l:./i2pd/libi2pdwrapper.a -l:../i2pd/libi2pd.a -l:../i2pd/libi2pdlang.a -latomic -lcrypto -lssl -lz -lboost_system -lboost_date_time -lboost_filesystem -lboost_program_options -lpthread -lstdc++
+// #cgo LDFLAGS: -L${SRCDIR}/../ -l:./i2pd/libi2pdclient.a -l:./i2pd/libi2pdwrapper.a -l:./i2pd/libi2pd.a -l:./i2pd/libi2pdlang.a -latomic -lcrypto -lssl -lz -lboost_system -lboost_date_time -lboost_filesystem -lboost_program_options -lpthread -lstdc++
 //
 // #include <stdlib.h>
 // #include "../i2pd/libi2pd_wrapper/capi.h"
